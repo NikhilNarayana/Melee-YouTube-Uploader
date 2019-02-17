9e4209cb67d2e1603d4d19aa61d43e717afb4a29
Nikhil Narayana <nikhil.narayana@live.com>
2019-02-17 14:12:07 -0500

    v1.10.1 Ask the user if they would like to update based on their version number and remove linux sudo call.

--- meleeuploader/main.py
+++ meleeuploader/main.py
@@ -2,29 +2,42 @@
 
 import os
 import sys
+import json
 import subprocess
+import pkg_resources
 
 from . import forms
 from . import consts
 from . import youtube as yt
 
+import requests
 import pyforms_lite
+
+__version__ = pkg_resources.require("MeleeUploader")[0].version
 
 
 def main():
-    subprocess.call(('pip3', 'install', '-U', 'meleeuploader'))
-    if "linux" in sys.platform:  # root needed for writing files
-        if os.geteuid() != 0:
-            print("Need sudo for writing files")
-            subprocess.call(['sudo', 'python3', sys.argv[0]])
-    # Always get the initial YT credentials outside of your app for safety.
+    try:
+        json_information = requests.get('https://pypi.python.org/pypi/meleeuploader/json').text
+        latest_version = json.loads(json_information)['info']['version']
+        if (__version__ != latest_version):
+            resp = input(f"Version {latest_version} is available, would you like to update? (y/n) ")
+            if any(x == resp for x in ("y", "yes", "Yes")):
+                subprocess.call(('pip3', 'install', '-U', 'meleeuploader'))
+                print("You can now restart the app to use the new version")
+    except Exception as e:
+        print("Error: " + e)
     try:
         consts.youtube = yt.get_youtube_service()
         consts.sheets = yt.get_spreadsheet_service()
-        pyforms_lite.start_app(forms.MeleeUploader, geometry=(200, 200, 1, 1))
     except Exception as e:
         print(e)
-        print("This program needs internet access to work")
+        print("There was an issue with getting Google Credentials")
+        sys.exit(1)
+    try:
+        pyforms_lite.start_app(forms.MeleeUploader, geometry=(200, 200, 1, 1))
+    except Exception as e:
+        print("Error:" + e)
         sys.exit(1)
 
 
