7bcd39c0ddaa112cc1f8a98a7bc877dac457d31e
Nikhil Narayana <nikhil.narayana@live.com>
2019-04-08 12:38:54 -0400

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-version = '1.11.1'
+version = '1.11.2'
 
 long_des = ""
 with open(path.join(here, 'README.md')) as f:
