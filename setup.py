d0cb88f3d8057cd36ee342613fe6bd61f5af9529
Nikhil Narayana <nikhil.narayana@live.com>
2022-01-24 10:31:28 -0800

    release: v1.21.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-version = "1.21.5"
+version = "1.21.6"
 
 long_des = ""
 with open(path.join(here, "README.md")) as f:
